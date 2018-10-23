ecea01d0f5b1bede31c919ff409a667bb739bb6f
GreenOk <greenok.scb@gmail.com>
2018-10-23 22:22:26 +0300

    Task 2

---
+++ src/HW2110/HW2Task2.java
@@ -0,0 +1,16 @@
+package HW2110;
+
+public class HW2Task2 {
+    public static void main(String[] args) {
+        /*Задача 3*
+            Создайте программу, выводящую на экран первые
+             20 элементов последовательности 2 4 8 16 32 64 128 ….
+             */
+        /*  */
+        int w = 2;
+        for (int q = 0; q < 21; q++) {
+            System.out.print(w + " ");
+            w *= 2;
+        }
+    }
+}
