d859fc56b799327c83a856c3c746649c72cbf801
GreenOk <greenok.scb@gmail.com>
2018-10-28 21:46:54 +0300

    HW2Task4

---
+++ src/HW2110/HW2Task4.java
@@ -0,0 +1,23 @@
+package HW2110;
+
+public class HW2Task4 {
+    public static void main(String[] args) {
+        /*Задача 4
+        Проверьте, является ли натуральное число — простым.
+        */
+        int a = 0;
+        int count = 0;
+        for (int i = a; i >= 1; i--) {
+            if (a % i == 0) {
+                count++;
+            }
+        }
+        if (count > 2) {
+            System.out.println("Заданное число - составное");
+        } else if (count == 2) {
+            System.out.println("Заданное число простое");
+        } else {
+            System.out.println("Ошибка. Введеное не число не является натуральным");
+        }
+    }
+}
